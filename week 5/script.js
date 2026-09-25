a4f40a72052586a6ab0629a20aa890593be36c0b
anguyen0344 <a.nguyen0344@student.sbccd.edu>
2026-09-24 17:49:00 -0700

    Delete week 5 directory

--- week 5/script.js
+++
@@ -1,9 +0,0 @@
-const tipButton = document.querySelector("#tip-button");
-const tipResult = document.querySelector("#tip-result");
-
-if (tipButton && tipResult) {
-  tipButton.addEventListener("click", () => {
-    tipResult.textContent =
-      "Beginner tip: Try changing one thing at a time, then check the result.";
-  });
-}
